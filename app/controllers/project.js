ab94caa3b24a0ea74f2c67496e85daf20112aefe
Tommy Bergeron <t.bergeron@gmail.com>
2012-04-05 04:14:19 +0000

    handlebars conversion, re-importing old stuff

---
+++ app/controllers/project.js
@@ -0,0 +1,91 @@
+var ProjectController = {
+  // properties
+  Projects: null,
+  
+  // constructor
+  initialize: function(req, res) {
+    //check_if_authorized(req, res);
+    
+    this.Projects = this.repositories.Projects
+    
+    var breadcrumbs = [];
+      breadcrumbs[0] = 'Projects|/projects';
+    // todo: this is kinda weird, you know?
+
+    return breadcrumbs
+  },
+  
+  render_form: function(req, res, project, breadcrumbs) {
+    // breadcrumbs
+    breadcrumbs[1] = ((project) ? project.name : 'Create a new project') + '|/projects'
+  
+    res.render('projects/edit', {
+      title: (project) ? 'Editing project: ' + project.name : 'Create a new project',
+      project: project,
+      breadcrumbs: breadcrumbs
+    })
+  },
+  
+  // GET: /projects
+  list: function(req, res) {
+    var breadcrumbs = this.initialize(req, res)
+
+  //projectRepository.allByDate(function(projects) {
+  //  if (projects) {
+  //    res.render('projects/list', {
+  //      title: 'List of projects',
+  //      projects: projects,
+  //      breadcrumbs: breadcrumbs
+  //    })
+  //  } else {
+  //    req.flash('error', 'No projects found.')
+  //    res.redirect('/')
+  //  }
+  //})
+  },
+  
+  new: function(req, res) {
+    this.initialize(req, res)
+    render_form(req, res)
+  },
+  
+  // GET&POST: /projects/edit/:project_code
+  edit: function(req, res) {
+    var breadcrumbs = this.initialize(req, res)
+    if (is_post(req)) {
+      // if this is POST, validates and saves the object.
+      //projectRepository.save(req.body.project, function(project, errors) {
+      //  if (errors) {
+      //    flashErrors(req, errors)
+      //  } else {
+      //    req.flash('success', 'Saved with success.')
+      //  }
+        render_form(req, res, project, breadcrumbs)
+      //})
+    } else {
+      // if this is no POST, then it's an edit form
+      //projectRepository.byCode(req.params.project_code, function(project) {
+      //  if (project) {
+      //    render_form(req, res, project, breadcrumbs)
+      //  } else {
+      //    req.flash('error', 'Unable to find project.')
+      //    res.redirect('/projects')
+      //  }
+      //})
+    }
+  },
+  
+  // GET: /projects/delete
+  delete: function(req, res) {
+    this.initialize(req, res)
+    //projectRepository.delete(req.params.project_code, function(err){
+    //  if (!err) {
+    //    req.flash('success', 'Project with code "'+ req.params.project_code +'" has been deleted with success.')
+    //  }
+    //  res.redirect('/projects')
+    //})
+  }
+
+}
+
+module.exports = ProjectController
---
+++ app/repositories/Projects.js
@@ -0,0 +1,93 @@
+var collection  = require("mongo-col"),
+    pd   		    = require("pd")
+
+var Projects = pd.extend(Object.create(collection("projects", "thinair")), {
+
+  // gets a list of all projects sorting by date_created ascending.
+  allByDate: function(callback) {
+    //db.projects.find().sort({date_created:1}, function(err, projects) {
+    //  if(!err || projects) {
+    //    callback(projects)
+    //  } else {
+    //    console.t.log('Unable to fetch objects: ' + err)
+    //  }
+    //})
+  },
+
+  // gets a project by its code
+  byCode: function(code, callback) {
+    //db.projects.findOne({ code: code }, function(err, project) {
+    //  if(!err || project) {
+    //    callback(project)
+    //  } else {
+    //    console.t.log('Unable to fetch object: ' + err)
+    //  }
+    //})
+  },
+
+  // saves a project
+  save: function(project, callback) {
+    // save object
+    //var project_code = slugify(project.name)
+  
+    // todo: CHECK FOR DUPLICATES
+    //validates('project', project, function(errors) {
+    //  if (!errors) {
+    //    if (isNew(project)) {
+          // if it's a new project, save it
+    //      db.projects.save({
+    //        code: project_code,
+    //        name: project.name
+    //      })
+  
+          // fetch the saved project
+    //      db.projects.findOne({ code: project_code }, function(err, new_project) {
+    //        return callback(new_project)
+    //      })
+  
+    //    } else {
+          // if it's not a new project, update the existing one
+    //      db.projects.update({ _id: db.ObjectId(project._id) }, { $set: {
+    //        code: project_code,
+    //        name: project.name
+    //      }})
+  
+          // fetch the created project
+    //      db.projects.findOne({ _id: db.ObjectId(project._id) }, function(err, updated_project) {
+    //        return callback(updated_project)
+    //      })
+    //    }
+    //  } else {
+        // if there's validation errors
+    //    if (isNew(project)) {
+          // if it's a new project, send it as it is
+    //      return callback(project, errors)
+  
+    //    } else {
+          // if it's an existing one, fetch it and put and replace the values with the on from the form
+    //      db.projects.findOne({ _id: db.ObjectId(project._id) }, function(err, fetched_project) {
+    //        fetched_project.code = project_code
+    //        fetched_project.name = project.name
+  
+    //        return callback(fetched_project, errors)
+    //      })
+    //    }
+    //  }
+  
+    //})
+  },
+
+  // deletes an object
+  delete: function(code, callback) {
+    //db.projects.remove({ code: code }, function(err) {
+    //  if (err) {
+    //    console.t.log('error', 'Unable to delete project.')
+    //  }
+  
+    //  callback(err)
+    //})
+  }
+
+})
+
+module.exports = Projects
