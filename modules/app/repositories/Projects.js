e5619e3708ce27833a29d5f124157b665036f473
Tommy Bergeron <t.bergeron@gmail.com>
2012-04-20 00:52:07 -0400

    made baseSave

--- modules/app/repositories/Projects.js
+++ modules/app/repositories/Projects.js
@@ -1,10 +1,10 @@
 var helpers          = require('../../libs/helpers'),
-    validator        = require('../../libs/validator'),
     createRepository = require('../../libs/repository').createRepository;
 
 var Projects = createRepository('projects', {
   // gets a list of all projects sorting by date_created ascending.
   allByDate: function(callback) {
+
     this.find().sort({ date_created: 1 }).toArray(function(err, projects) {
       if (err) console.log(err);
 
@@ -31,53 +31,12 @@ var Projects = createRepository('projects', {
 
   // saves a project
   save: function(project, callback) {
-    var project_code = helpers.slugify(project.name),
-        that = this;
-
-    console.log('why is this being executed a fuck load of times by tests, wtf?');
+    project.code = helpers.slugify(project.name);
 
     //todo: CHECK FOR DUPLICATES
-    validator.validate('project', project, function(errors) {
-      if (!errors) {
-        if (helpers.isNew(project)) {
-          //if it's a new project, save it
-          that.save({ code: project_code, name: project.name });
-          //fetch the saved project
-          that.findOne({ code: project_code }, function(err, new_project) {
-            if (err) console.log(err);
 
-            if (helpers.isDefined(callback) && helpers.isDefined(new_project)) {
-              return callback(new_project);
-            }
-          });
-        } else {
-          //if it's not a new project, update the existing one
-          that.update({ _id: that.ObjectId(project._id) }, {
-            $set: { code: project_code, name: project.name }
-          });
-          //fetch the created project
-          that.findOne({ _id: that.ObjectId(project._id) }, function(err, updated_project) {
-            return callback(updated_project);
-          });
-        }
-      } else {
-        //if there's validation errors
-        if (helpers.isNew(project)) {
-          //if it's a new project, send it as it is
-          return callback(project, errors);
-        } else {
-          //if it's an existing one, fetch it and put and replace the values with the on from the form
-          that.findOne({ _id: that.ObjectId(project._id) }, function(err, fetched_project) {
-            if (fetched_project) {
-              fetched_project.code = project_code;
-              fetched_project.name = project.name;
-              return callback(fetched_project, errors);
-            } else {
-              return callback(null, errors);
-            }
-          });
-        }
-      }
+    this.baseSave(project, function(savedProject, errors) {
+      return callback(savedProject, errors);
     });
   },
 
--- modules/libs/repository.js
+++ modules/libs/repository.js
@@ -1,10 +1,30 @@
-var collection  = require('mongo-col'),
-    pd          = require('pd');
+var helpers          = require('../../libs/helpers'),
+    validator        = require('../../libs/validator'),
+    collection       = require('mongo-col'),
+    pd               = require('pd');
 
 var Repository = {
   createRepository: function(name, content) {
-    return pd.extend(Object.create(collection(name, process.env["MONGODB_DATABASE"])), content, {
-        ObjectId: require('mongodb').ObjectID
+    var db = collection(name, process.env["MONGODB_DATABASE"]);
+
+    return pd.extend(Object.create(db), content, {
+        ObjectId: require('mongodb').ObjectID,
+
+        baseSave: function (object, callback) {
+          validator.validate(name, object, function(errors) {
+            if (errors) {
+              return callback(object, errors);
+            } else {
+              db.save(object, function(err) {
+                if (err) {
+                  console.log(err);
+                  return callback(object, err);
+                }
+              });
+            }
+          });
+        }
+
     });
   }
 };
