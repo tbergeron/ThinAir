80dd3850889497e4190b645849f3c90e5a176761
Tommy Bergeron <t.bergeron@gmail.com>
2012-05-03 09:45:00 -0400

    pushed ignores

--- public/js/thinair.js
+++
@@ -1,7 +0,0 @@
-$(function(){
-	var currentUri = window.location.pathname;
-	var elementName = currentUri.replace(/^\/([^\/]*).*$/, '$1');
-
-	// selected element from the navigation
-	$('ul.nav li.' + ((elementName.length > 0) ? elementName : 'home')).addClass('active');
-});
