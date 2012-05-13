141e3e1340bfdc50310bed2bb41c44738d701855
Tommy Bergeron <t.bergeron@gmail.com>
2012-05-13 14:14:28 -0400

    testing new ncore

--- core.js
+++ core.js
@@ -1,2 +1,9 @@
 require("./db");
-require("ncore/modules/moduleLoader").core(require("path").join(__dirname, "modules"));
+//require("ncore/modules/moduleLoader").core(require("path").join(__dirname, "modules"));
+
+require("ncore/modules/core")({
+    uri: require("path").join(__dirname, "app"),
+    dependencyMapper: {
+        jsonUri: require("path").join(__dirname, "dependency.json")
+    }
+});
