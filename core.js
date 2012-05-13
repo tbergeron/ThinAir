3845bc2d4acac4e45f94f335afa13ae8833c1e2b
Tommy Bergeron <t.bergeron@gmail.com>
2012-05-13 18:06:11 -0400

    that's how I think it sould work

--- core.js
+++ core.js
@@ -2,8 +2,8 @@ require("./db");
 //require("ncore/modules/moduleLoader").core(require("path").join(__dirname, "modules"));
 
 require("ncore/modules/core")({
-    uri: require("path").join(__dirname, "app"),
+    uri: require("path").join(__dirname, "libs"),
     dependencyMapper: {
-        jsonUri: require("path").join(__dirname, "dependency.json")
+        jsonUri: require("path").join(__dirname, "libs", "dependency.json")
     }
 });
