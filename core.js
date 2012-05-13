55cb98b0fac0628b8749375ebc0f5ad98f6af38c
Tommy Bergeron <t.bergeron@gmail.com>
2012-05-13 15:26:41 -0400

    infinite loopin'

--- core.js
+++ core.js
@@ -2,7 +2,7 @@ require("./db");
 //require("ncore/modules/moduleLoader").core(require("path").join(__dirname, "modules"));
 
 require("ncore/modules/core")({
-    uri: require("path").join(__dirname, "/"),
+    uri: require("path").join(__dirname, "app"),
     dependencyMapper: {
         jsonUri: require("path").join(__dirname, "dependency.json")
     }
