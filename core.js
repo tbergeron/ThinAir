c8305067577d6ffb1c6b3717106857aff7f3e75a
Tommy Bergeron <t.bergeron@gmail.com>
2012-05-13 15:23:14 -0400

    wtf describe is not defined

--- core.js
+++ core.js
@@ -2,7 +2,7 @@ require("./db");
 //require("ncore/modules/moduleLoader").core(require("path").join(__dirname, "modules"));
 
 require("ncore/modules/core")({
-    uri: require("path").join(__dirname, "app"),
+    uri: require("path").join(__dirname, "/"),
     dependencyMapper: {
         jsonUri: require("path").join(__dirname, "dependency.json")
     }
