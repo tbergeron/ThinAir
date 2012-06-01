8ca19c8d68a953bf752aa864cec0140ddc391ad7
Tommy <tomshreds@me.com>
2012-06-01 00:57:23 +0000

    db.js is now config.js

--- db.js
+++
@@ -1,5 +0,0 @@
-process.env['MONGODB_HOST'] = 'staff.mongohq.com';
-process.env["MONGODB_PORT"] = 10063;
-process.env['MONGODB_DATABASE'] = 'thinair';
-process.env["MONGODB_USERNAME"] = 'thinair';
-process.env["MONGODB_PASSWORD"] = 'helloworld';
