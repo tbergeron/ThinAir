3f1f519df12632cc7144cdf456d3581cf78cf553
Tommy <tomshreds@me.com>
2012-06-01 00:21:46 +0000

    working fine now starting to merge

--- libs/configure.js
+++ libs/configure.js
@@ -18,7 +18,7 @@ module.exports = {
       app.set("views", __dirname + "/../app/views");
       app.use(express.favicon());
       app.use(express.logger("dev"));
-      app.use(express["static"](__dirname + "/../../public"));
+      app.use(express["static"](__dirname + "/../public"));
       app.use(express.bodyParser());
       app.use(express.methodOverride());
 
