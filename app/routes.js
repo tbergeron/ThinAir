430dd4fecf79747da7cd5f17aafd4656ccac271c
Tommy Bergeron <t.bergeron@gmail.com>
2012-07-07 13:25:49 -0400

    removed repositories support temporarly, this is a very lightweight version of thinair right now

--- app/routes.js
+++ app/routes.js
@@ -1,8 +1,9 @@
 module.exports = {
   registerRoutes: function() {
     var router = this.router,
-        getAction = this.router.getAction,
-        home      = this.controllers.home;
+        getAction = this.router.getAction;
+
+    var home = this.controllers.home;
 
     router.add("/", getAction(home, "index"));
   }
