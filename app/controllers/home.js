627b8ca531792b68597c24914a732eb10d5c4573
Tommy Bergeron <t.bergeron@gmail.com>
2012-07-07 17:30:07 -0400

    less is more

--- app/controllers/home.js
+++ app/controllers/home.js
@@ -1,9 +1,7 @@
 var ThinAir = require("../../libs/thinair");
 
-var HomeController = ThinAir.createController({
+module.exports = ThinAir.createController({
     index: function(req, res, params) {
         this.sendHtml(res, '<strong>hello world</strong>');
     }
 });
-
-module.exports = HomeController;
