963c57322c42dd749aa6c07e960a3e18c5cc7e3d
Tommy Bergeron <t.bergeron@gmail.com>
2012-07-06 22:23:05 -0400

    removed useless stuff

--- app/controllers/home.js
+++ app/controllers/home.js
@@ -3,13 +3,12 @@ var createController = require("../../libs/controllers").createController;
 var HomeController = createController({
     // /projects
     index: function(req, res) {
-        // todo: checks for express 3 replacement?
-        res.render("index", { title: "ThinAir" });
+        // res.render("index", { title: "ThinAir" });
     },
 
     // every 404
     error: function(req, res) {
-        res.render("error", { title: "Error" });
+        // res.render("error", { title: "Error" });
     }
 });
 
