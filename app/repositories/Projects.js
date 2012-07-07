a1fe558c3a5d0e30331fb88af43b0d880aad93fa
Tommy Bergeron <t.bergeron@gmail.com>
2012-07-07 19:54:27 -0400

    restructured repositories

--- app/repositories/Projects.js
+++ app/repositories/Projects.js
@@ -1,19 +1,6 @@
-var strings = require("../../libs/helpers/strings"),
-    createRepository = require("../../libs/repositories").createRepository;
+var ThinAir = require('../../libs/thinair');
 
-var Projects = createRepository("projects", {
-    init: function() {
-        // initializing a reactive method
-        var that = this;
-        this.sockets.createReactiveMethod('getProject', function(parameters, callback) {
-            if (parameters.code) {
-                that.getByCode(parameters.code, function(project) {
-                    callback(project);
-                });
-            }
-        });
-    },
-
+module.exports = ThinAir.createRepository("projects", {
     // gets a list of project, sorted by date_created
     getAllByDate: function(callback) {
         this.find().sort({ date_created: 1 }).toArray(function(err, projects) {
@@ -21,30 +8,4 @@ var Projects = createRepository("projects", {
             return callback(projects ? projects : null);
         });
     },
-
-    // gets one project by its code
-    getByCode: function(code, callback) {
-        this.baseFindOne({ code: code }, function(project) {
-            return callback(project ? project : null);
-        });
-    },
-
-    // saves a project
-    save: function(project, callback) {
-        // converts the project's code to a slug
-        project.code = strings.slugify(project.name);
-
-        this.baseSave(project, function(savedProject, errors) {
-            return callback(savedProject, errors);
-        });
-    },
-
-    // deletes a project by its ObjectID
-    delete: function(objectId, callback) {
-        this.baseDelete({ _id: this.ObjectId(objectId) }, function(success, project) {
-            return callback(success, project);
-        });
-    }
 });
-
-module.exports = Projects;
