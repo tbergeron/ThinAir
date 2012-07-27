a76e0ab0b4f1e53e5644d214e637bf1fc16d1a1b
Tommy Bergeron <t.bergeron@gmail.com>
2012-07-27 10:54:24 -0400

    committed wrong file wtf

--- libs/controllers.js
+++ libs/controllers.js
@@ -1,90 +1,50 @@
-var ThinAir = require('../../libs/thinair');
+var pd = require('pd'),
+    routil = require('routil'),
+    session = require('routil-session')(),
+    send = require('routil-send');
 
-module.exports = ThinAir.createController({
-    Lists: null,
+var Controllers = {
+    // creates a basic controller
+    createController: function(content) {
+        return pd.extend(Object.create({}), routil, send, content, {
+            sessions: session,
 
-    setup: function(done) {
-        this.Lists = this.repositories.Lists;
+            // is a GET request?
+            isGet: function(req) {
+                if (req.method === 'GET') {
+                    return true;
+                } else {
+                    return false;
+                }
+            },
 
-        // never forget to tell when the controller has finished setup
-        done();
-    },
+            // is a POST request?
+            isPost: function(req) {
+                if (req.method === 'POST') {
+                    return true;
+                } else {
+                    return false;
+                }
+            },
 
-    index: function(req, res, params) {
-        var that = this;
+            // is the request an AJAX call?
+            isXHR: function(req, res) {
+                if (req.header('HTTP_X_REQUESTED_WITH') === 'XMLHttpRequest') {
+                    return true;
+                } else {
+                    return false;
+                }
+            },
 
-        this.Lists.getAll(function(lists) {
-            that.sendTemplate(req, res, 'lists/list', { lists: lists });
-        });
-    },
+            // sends a template
+            sendTemplate: function(req, res, name, params) {
+                // preventing templar to throw an error if there's no data
+                if (!params) params = {};
 
-    add: function(req, res, params) {
-        this.sendTemplate(req, res, 'lists/edit', { addMode: true });
-    },
-
-    edit: function(req, res, params) {
-        var that = this;
-
-        // response from the form
-        if (this.isPost(req)) {
-
-            var list = params.post.list;
-
-            list.items = [];
-
-            that.Lists.save(params.post.list, function(list, errors) {
-                var message = (errors) ? errors[0] : 'success'
-                    opts = { message: message, list: list };
-
-                that.sendTemplate(req, res, 'lists/edit', opts);
-            });
-        } else {
-            this.Lists.getOneByCode(params.list_code, function(list) {
-                that.sendTemplate(req, res, 'lists/edit', { list: list });
-            });
-        }
-    },
-
-    delete: function(req, res, params) {
-        var that = this;
-
-        this.Lists.delete(params.list_code, function(err) {
-            // TODOTB: how to pass this while redirect? { messages: 'List deleted succesfully.'}
-            that.redirect(req, res, "/lists");
-        });
-    },
-
-    getItems: function(req, res, params) {
-        var that = this;
-
-        this.Lists.getOneByCode(params.list_code, function(list) {
-            that.sendJson(res, list.items);
-        });
-    },
-
-    addItem: function(req, res, params) {
-        var that = this;
-
-        if (this.isPost(req)) {
-            if (params.post.list_code) {
-                // getting list
-                this.Lists.getOneByCode(params.post.list_code, function(list) {
-                    if (list) {
-                        // adding item to list
-                        list.items.push(params.post.item);
-
-                        // saving list
-                        that.Lists.save(list, function(list, errors) {
-                            that.sendHtml(res, 'true');
-                        });
-                    }
-                });
-            } else {
-                this.sendHtml(res, 'false');
+                return this.template(req, res, name + '.html', params);
             }
-        } else {
-            this.errorPage(req, res, 403);
-        }
+        });
     }
+};
 
-});
+module.exports = Controllers;
