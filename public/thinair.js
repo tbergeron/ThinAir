d02d88c84965d7afd3bc7af59e18ce159b1ca4df
Tommy Bergeron <t.bergeron@gmail.com>
2012-07-28 20:46:24 -0400

    re-added thinair.js back #35 added an auto update script

---
+++ public/thinair.js
@@ -0,0 +1,8 @@
+var socket = io.connect();
+
+// calls a reactive method
+var callReactiveMethod = function(name, parameters, callback) {
+	socket.emit(name, parameters, function (data) {
+		callback(data);
+	});
+};
