ea7f624cb618aa5086eda04fdfb3d0cae8d23f07
Tommy Bergeron <t.bergeron@gmail.com>
2012-08-05 20:06:22 -0400

    fixed a static file bug

--- libs/router.js
+++ libs/router.js
@@ -4,14 +4,15 @@ var static = require('node-static'),
     isDefined = require('./thinair').isDefined,
     pd = require('pd'),
     qsObjects = require('qs-objects'),
-    formidable = require('formidable');
+    formidable = require('formidable'),
+    path = require('path');
 
 
 // change the path if it's called from tests
 if (process.env.CALLED_FROM_TESTS) {
-    var publicPath = '../../public';
+    var publicPath = path.join(__dirname, '../../public');
 } else {
-    var publicPath = './public';
+    var publicPath = path.join(__dirname, '../public');
 }
 
 var file = new(static.Server)(publicPath);
