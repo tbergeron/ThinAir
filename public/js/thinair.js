a53681366f390cb5f9557af4fbe8201d3af25e2d
Tommy Bergeron <t.bergeron@gmail.com>
2012-08-17 13:06:37 -0400

    moved cli to bin

--- public/js/thinair.js
+++
@@ -1,8 +0,0 @@
-var socket = io.connect();
-
-// calls a reactive method
-var callReactiveMethod = function(name, parameters, callback) {
-	socket.emit(name, parameters, function (data) {
-		callback(data);
-	});
-};
