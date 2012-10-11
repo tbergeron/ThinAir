24e4275f2d3f46fd8a8f20de3e4c3620ccdd303d
Tommy Bergeron <t.bergeron@gmail.com>
2012-10-11 11:01:49 -0400

    test

--- libs/server.js
+++ libs/server.js
@@ -17,6 +17,7 @@ requestHandler.startServer = function() {
         var MongoStore = require('connect-mongodb');
 
         var mongoStoreOptions = {
+            safe: true,
             url: 'mongodb://' + process.env.MONGODB_USER + ':' + process.env.MONGODB_PASSWORD + '@' + process.env.MONGODB_HOST + ':' + process.env.MONGODB_PORT + '/' + process.env.MONGODB_DATABASE
         };
 
